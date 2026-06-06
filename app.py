0c3872d1653fc6cd222ed80e3f359deac80c4121
Muchumarri Hemalatha <hemalathamuchumarri@gmail.com>
2026-06-06 18:18:04 +0530

    Remove version pins to fix dependency conflicts

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
